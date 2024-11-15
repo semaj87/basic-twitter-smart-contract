d196704a18cf5a47e7a3d413783432bc1dc6bb74
James Aymer <aymerjames@gmail.com>
2024-11-15 16:02:14 +0000

    WIP - limiting the size of a tweet

--- contracts/Twitter.sol
+++ contracts/Twitter.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.26;
 
 contract Twitter {
 
+    uint16 constant private MAX_TWEET_LENGTH = 280;
+
     struct Tweet {
         address author;
         string content;
@@ -14,6 +16,8 @@ contract Twitter {
     mapping(address => Tweet[]) public tweets;
 
     function createTweet(string memory _tweet) public {
+        require(bytes(_tweet) <= MAX_TWEET_LENGTH, "Your Tweet was too long!");
+
         Tweet memory newTweet = Tweet({
             author: msg.sender,
             content: _tweet,
@@ -24,8 +28,8 @@ contract Twitter {
         tweets[msg.sender].push(newTweet);
     }
 
-    function getTweet(address _ownerAddress, uint _index) public view returns (Tweet memory) {
-        return tweets[_ownerAddress][_index];
+    function getTweet(uint _index) public view returns (Tweet memory) {
+        return tweets[msg.sender][_index];
     }
 
     function getAllTweets(address _ownerAddress) public view returns (Tweet[] memory) {
