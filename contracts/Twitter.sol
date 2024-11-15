4d32cda4d24cba951978b4739dab05051b61d06a
James Aymer <aymerjames@gmail.com>
2024-11-15 15:18:15 +0000

    WIP - small change to use structs

--- contracts/Twitter.sol
+++ contracts/Twitter.sol
@@ -4,17 +4,31 @@ pragma solidity ^0.8.26;
 
 contract Twitter {
 
-    mapping(address => string[]) public tweets;
-
-    function createTweet(string memory _tweet) public {
-        tweets[msg.sender].push(_tweet);
+    struct Tweet {
+        address author;
+        string content;
+        uint256 timestamp;
+        uint256 likes;
     }
 
-    function getTweet(address _ownerAddress, uint _index) public view returns (string memory) {
+    mapping(address => Tweet[]) public tweets;
+
+    function createTweet(string memory _tweet) public {
+        Tweet memory newTweet = Tweet({
+            author: msg.sender,
+            content: _tweet,
+            timestamp: block.timestamp,
+            likes: 0
+        });
+
+        tweets[msg.sender].push(newTweet);
+    }
+
+    function getTweet(address _ownerAddress, uint _index) public view returns (Tweet memory) {
         return tweets[_ownerAddress][_index];
     }
 
-    function getAllTweets(address _ownerAddress) public view returns (string[] memory) {
+    function getAllTweets(address _ownerAddress) public view returns (Tweet[] memory) {
         return tweets[_ownerAddress];
     }
 }
