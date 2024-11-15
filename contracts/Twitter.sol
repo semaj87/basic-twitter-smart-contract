7604f8439e5b774a6f42abff9f954c9cb2054ce7
James Aymer <aymerjames@gmail.com>
2024-11-15 14:39:35 +0000

    WIP - small array change

--- contracts/Twitter.sol
+++ contracts/Twitter.sol
@@ -4,13 +4,17 @@ pragma solidity ^0.8.26;
 
 contract Twitter {
 
-    mapping(address => string) public tweets;
+    mapping(address => string[]) public tweets;
 
     function createTweet(string memory _tweet) public {
-        tweets[msg.sender] = _tweet;
+        tweets[msg.sender].push(_tweet);
     }
 
-    function getTweet(address _ownerAddress) public view returns (string memory) {
+    function getTweet(address _ownerAddress, uint _index) public view returns (string memory) {
+        return tweets[_ownerAddress][_index];
+    }
+
+    function getAllTweets(address _ownerAddress) public view returns (string[] memory) {
         return tweets[_ownerAddress];
     }
 }
